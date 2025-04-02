0e269e6c9ad0565281dec28eb4a2017e1815d65f
Ensar Mulahmetović <ensar.mulahmetovic@edu.fit.ba>
2025-04-02 23:53:33 +0200

    C2 - Started implementing base interface and service

--- eCommerce/eCommerce.Services/IProductTypeService.cs
+++ eCommerce/eCommerce.Services/IProductTypeService.cs
@@ -7,10 +7,9 @@ using eCommerce.Model.SearchObjects;
 
 namespace eCommerce.Services
 {
-    public interface IProductTypeService
+    public interface IProductTypeService:IService<ProductType, ProductTypeSearchObject>
     {
-        Task<List<ProductTypeResponse>> GetAsync(ProductTypeSearchObject search);
-        Task<ProductTypeResponse?> GetByIdAsync(int id);
+       
         Task<ProductTypeResponse> CreateAsync(ProductTypeUpsertRequest request);
         Task<ProductTypeResponse?> UpdateAsync(int id, ProductTypeUpsertRequest request);
         Task<bool> DeleteAsync(int id);
---
+++ eCommerce/eCommerce.Services/IService.cs
@@ -0,0 +1,18 @@
+﻿using eCommerce.Model.Requests;
+using eCommerce.Model.Responses;
+using eCommerce.Model.SearchObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.Services
+{
+    public interface IService<T, TSearch> where T : class where TSearch: class
+    {
+        Task<List<T>> GetAsync(TSearch search);
+        Task<T?> GetByIdAsync(int id);
+     
+    }
+}
